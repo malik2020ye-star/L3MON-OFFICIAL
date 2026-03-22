99093c543cc8ad229056cc8f12f402bdcc88f20f
malik2020ye-star <malik2020ye@gmail.com>
2026-03-22 20:46:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-// AUTO REMOVE INDEX.JS CONTENTS //
-console.log("This project is coming soon! Please check back later.")
-process.exit(0)
-// AUTO REMOVE INDEX.JS CONTENTS //
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 8080;
+
+// محرك التشغيل الأصلي لـ L3MON
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.redirect('/login');
+});
+
+app.get('/login', (req, res) => {
+    // هذا سيفتح لك واجهة الدخول الأصلية
+    res.sendFile(path.join(__dirname, 'src/includes/login.html'));
+});
+
+app.listen(port, () => {
+    console.log(`L3MON Server is running on port ${port}`);
+});
