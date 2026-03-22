88446f29781696aedef08b6587807832844b2f8f
malik2020ye-star <malik2020ye@gmail.com>
2026-03-22 22:08:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 10000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'src/includes')));
 
 // الصفحة الرئيسية تحولك للوجن
-app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'src/includes/login.html')); });
+app.get('/', (req, res) => { res.sendhttps://github.com/malik2020ye-star/L3MON-OFFICIAL/edit/main/src/index.jsFile(path.join(__dirname, 'src/includes/login.html')); });
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'src/includes/login.html'));
 });
