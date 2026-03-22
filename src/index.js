f76b02288d0fb39522148924ad79944071dd35fc
malik2020ye-star <malik2020ye@gmail.com>
2026-03-22 22:13:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ const port = process.env.PORT || 10000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'src/includes')));
 
-// الصفحة الرئيسية تحولك للوجن
-app.get('/', (req, res) => { res.sendhttps://github.com/malik2020ye-star/L3MON-OFFICIAL/edit/main/src/index.jsFile(path.join(__dirname, 'src/includes/login.html')); });
+// الصفحة الرئيسية تعرض صفحة اللوجن مباشرة
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/includes/login.html'));
+});
+
+// صفحة اللوجن (للاحتياط)
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'src/includes/login.html'));
 });
@@ -15,11 +19,10 @@ app.get('/login', (req, res) => {
 // معالجة بيانات الدخول
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
-    // اليوزر admin والباسورد 123456
     if (username === 'admin' && password === '123456') {
-        res.send('<h1 style="color:green; text-align:center;">تم تسجيل الدخول بنجاح! أهلاً بك في لوحة التحكم الكاملة.</h1>');
+        res.send('<h1 style="color:green; text-align:center; font-family:sans-serif; margin-top:50px;">تم تسجيل الدخول بنجاح! أهلاً بك في لوحة التحكم.</h1>');
     } else {
-        res.send('<h1 style="color:red; text-align:center;">خطأ في البيانات! حاول مرة أخرى.</h1><a href="/login">رجوع</a>');
+        res.send('<h1 style="color:red; text-align:center; font-family:sans-serif; margin-top:50px;">خطأ في البيانات!</h1><p style="text-align:center;"><a href="/">رجوع</a></p>');
     }
 });
 
